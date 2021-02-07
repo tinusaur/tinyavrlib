80c43281cfa3cc5efee74cfa30ee4d1f6a604b6f
Neven Boyanov <boyanov@msn.com>
2021-02-07 16:50:26 +0200

    Updated the TinyAVRLib/Scheduler Testing scripts.

--- tinyavrlib_scheduler1/main.c
+++ tinyavrlib_scheduler1/main.c
@@ -1,106 +1,88 @@
 /**
- * Scheduler - Testing script.
- *
- * @created 2016-05-15
+ * TinyAVRLib/Scheduler - Testing scripts
  * @author Neven Boyanov
- *
  * This is part of the Tinusaur/TinyAVRLib project.
- *
- * Copyright (c) 2017 Neven Boyanov, The Tinusaur Team. All Rights Reserved.
- * Distributed as open source software under MIT License, see LICENSE.txt file.
- * Retain in your source code the link http://tinusaur.org to the Tinusaur project.
- *
- * Source code available at: https://bitbucket.org/tinusaur/tinyavrlib
- *
+ * ----------------------------------------------------------------------------
+ *  Copyright (c) 2021 Tinusaur (https://tinusaur.com). All rights reserved.
+ *  Distributed as open source under the MIT License (see the LICENSE.txt file)
+ *  Please, retain in your work a link to the Tinusaur project website.
+ * ----------------------------------------------------------------------------
+ * Source code available at: https://gitlab.com/tinusaur/tinyavrlib
  */
 
-// ============================================================================
-
-// #define F_CPU 1000000UL
-// NOTE: The F_CPU (CPU frequency) should not be defined in the source code.
-//       It should be defined in either (1) Makefile; or (2) in the IDE. 
-
-#include <stdlib.h>
+#include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
 #include "tinyavrlib/scheduler.h"
 
-#include "owowod/owowod.h"
-#include "owowod/debugging.h"
+// NOTE: To remove the use of the DEBUGGING: (1) remove its use from this file; (2) remove it (owowod and num2str) from the Makefile.
+// #include "owowod/owowod.h"
+// #include "owowod/debugging.h"
 
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                 ATtiny
-//                25/45/85
-//              +----------+   (-)--GND--
-//      (RST)---+ PB5  Vcc +---(+)--VCC--
-// --[OWOWOD]---+ PB3  PB2 +-------LED3--
-// --LED5-------+ PB4  PB1 +-------LED2--
-// -------(-)---+ GND  PB0 +-------LED1--
-//              +----------+
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                 ATtiny85
+//               +----------+    (-)--GND--
+//      (RST)--> + PB5  Vcc +----(+)--VCC--
+//   [OWOWOD]--> + PB3  PB2 +----LED2------
+// ------LED4----+ PB4  PB1 +----LED1------
+// -------(-)----+ GND  PB0 +----LED0------
+//               +----------+
+//                 Tinusaur
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ----------------------------------------------------------------------------
-
-#define LED1_PORT PB0
-#define LED2_PORT PB1
-#define LED3_PORT PB2
-// #define LED4_PORT PB3	// NOTE: Used by the OWOWOD library
-#define LED5_PORT PB4
+#define LED0_PORT PB0
+#define LED1_PORT PB1
+#define LED2_PORT PB2
+// #define LED3_PORT PB3	// NOTE: Used by the OWOWOD library
+#define LED4_PORT PB4
 
 // ----------------------------------------------------------------------------
 
 void scheduler_func0(uint32_t scheduler_tick) {
-	DEBUGGING_NUMDECUP(scheduler_tick);
+	// DEBUGGING_NUMDECUP(scheduler_tick);
 	uint8_t tick = scheduler_tick & 0b00000011;	// Mask the lower 2 bits.
-	DEBUGGING_NUMDECUP(tick);
+	// DEBUGGING_NUMDECUP(tick);
 	switch (tick) {
 		case 0:
-			PORTB ^= (1 << LED1_PORT);	// Flip the bit
+			PORTB ^= (1 << LED0_PORT);	// Flip the bit
 		break;
 		case 1:
-			PORTB ^= (1 << LED2_PORT);	// Flip the bit
+			PORTB ^= (1 << LED1_PORT);	// Flip the bit
 		break;
 		case 2:
-			PORTB ^= (1 << LED3_PORT);	// Flip the bit
+			PORTB ^= (1 << LED2_PORT);	// Flip the bit
 		break;
 		case 3:
-			PORTB ^= (1 << LED5_PORT);	// Flip the bit
+			PORTB ^= (1 << LED4_PORT);	// Flip the bit
 		break;
 	}
-	DEBUGGING_CRLF();
+	// DEBUGGING_CRLF();
 }
 
 // ----------------------------------------------------------------------------
 
 int main(void) {
-
-	// ---- Initialization ----
-	DEBUGGING_INIT();
-	DEBUGGING_CRLF(); DEBUGGING_STRINGLN("HELLO[SCHEDULER]");	// Not really needed
-
+	// ---- Init ----
+	// DEBUGGING_INIT();
+	// DEBUGGING_CRLF(); DEBUGGING_STRINGLN("HELLO[SCHEDULER]");	// Not really needed
 	scheduler_init(scheduler_func0);
 	scheduler_reinit(SCHEDULER_TCCR0B_1024, SCHEDULER_OCR0A_MAX);
 	
-	// Setup LED, DDRB - Data Direction Register, Port B
+	// Setup LEDs, DDRB - Data Direction Register, Port B
+	DDRB |= (1 << LED0_PORT); // Set port as LED output
 	DDRB |= (1 << LED1_PORT); // Set port as LED output
 	DDRB |= (1 << LED2_PORT); // Set port as LED output
-	DDRB |= (1 << LED3_PORT); // Set port as LED output
-	// DDRB |= (1 << LED4_PORT); // 	// NOTE: Used by the OWOWOD library
-	DDRB |= (1 << LED5_PORT); // Set port as LED output
+	// DDRB |= (1 << LED3_PORT); // 	// NOTE: Used by the OWOWOD library
+	DDRB |= (1 << LED4_PORT); // Set port as LED output
 
 	// Start the scheduler
 	scheduler_start();
 
 	// ---- Main Loop ----
-	for (;;) {
-		/* NOTING TO DO HERE */
-	}
+	for (;;) { /* The infinite main loop (NOTING TO DO) */ }
 
-	return 0;
+	return 0; // Return the mandatory for the "main" function int value - "0" for success.
 }
 
 // ============================================================================
--- tinyavrlib_scheduler2/main.c
+++ tinyavrlib_scheduler2/main.c
@@ -1,23 +1,14 @@
 /**
- * Scheduler - Testing script.
- *
+ * TinyAVRLib/Scheduler - Testing scripts
  * @author Neven Boyanov
- *
  * This is part of the Tinusaur/TinyAVRLib project.
- *
- * Copyright (c) 2018 Neven Boyanov, Tinusaur Team. All Rights Reserved.
- * Distributed as open source software under MIT License, see LICENSE.txt file.
- * Retain in your source code the link http://tinusaur.org to the Tinusaur project.
- *
- * Source code available at: https://bitbucket.org/tinusaur/tinyavrlib
- *
+ * ----------------------------------------------------------------------------
+ *  Copyright (c) 2021 Tinusaur (https://tinusaur.com). All rights reserved.
+ *  Distributed as open source under the MIT License (see the LICENSE.txt file)
+ *  Please, retain in your work a link to the Tinusaur project website.
+ * ----------------------------------------------------------------------------
+ * Source code available at: https://gitlab.com/tinusaur/tinyavrlib
  */
-
-// ============================================================================
-
-// #define F_CPU 1000000UL
-// NOTE: The F_CPU (CPU frequency) should not be defined in the source code.
-//       It should be defined in either (1) Makefile; or (2) in the IDE. 
 
 #include <stdint.h>
 #include <avr/io.h>
@@ -25,83 +16,75 @@
 
 #include "tinyavrlib/scheduler.h"
 
-#include "owowod/owowod.h"
-#include "owowod/debugging.h"
+// NOTE: To remove the use of the DEBUGGING: (1) remove its use from this file; (2) remove it (owowod and num2str) from the Makefile.
+// #include "owowod/owowod.h"
+// #include "owowod/debugging.h"
 
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                 ATtiny
-//                25/45/85
-//              +----------+   (-)--GND--
-//      (RST)---+ PB5  Vcc +---(+)--VCC--
-// --[OWOWOD]---+ PB3  PB2 +-------LED3--
-// --LED5-------+ PB4  PB1 +-------LED2--
-// -------(-)---+ GND  PB0 +-------LED1--
-//              +----------+
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                 ATtiny85
+//               +----------+    (-)--GND--
+//      (RST)--> + PB5  Vcc +----(+)--VCC--
+//   [OWOWOD]--> + PB3  PB2 +----LED2------
+// ------LED4----+ PB4  PB1 +----LED1------
+// -------(-)----+ GND  PB0 +----LED0------
+//               +----------+
+//                 Tinusaur
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ----------------------------------------------------------------------------
-
-#define LED1_PORT PB0
-#define LED2_PORT PB1
-#define LED3_PORT PB2
-// #define LED4_PORT PB3	// NOTE: Used by the OWOWOD library
-#define LED5_PORT PB4
+#define LED0_PORT PB0
+#define LED1_PORT PB1
+#define LED2_PORT PB2
+// #define LED3_PORT PB3	// NOTE: Used by the OWOWOD library
+#define LED4_PORT PB4
 
 // ----------------------------------------------------------------------------
 
 void scheduler_func0(uint32_t scheduler_tick) {
-	DEBUGGING_VARU("userfunc, tick", scheduler_tick); DEBUGGING_CRLF();
-	PORTB ^= (1 << LED5_PORT);	// Flip the bit
+	// DEBUGGING_VARU("userfunc, tick", scheduler_tick); DEBUGGING_CRLF();
+	PORTB ^= (1 << LED4_PORT);	// Flip the bit
 }
 
 void scheduler_task1(scheduler_status_p scheduler) {
-	DEBUGGING_VARU("task1, tick", (*scheduler).tick); DEBUGGING_CRLF();
-	PORTB ^= (1 << LED1_PORT);	// Flip the bit
+	// DEBUGGING_VARU("task1, tick", (*scheduler).tick); DEBUGGING_CRLF();
+	PORTB ^= (1 << LED0_PORT);	// Flip the bit
 }
 
 void scheduler_task2(scheduler_status_p scheduler) {
-	DEBUGGING_VARU("task2, tick", (*scheduler).tick); DEBUGGING_CRLF();
-	PORTB ^= (1 << LED2_PORT);	// Flip the bit
+	// DEBUGGING_VARU("task2, tick", (*scheduler).tick); DEBUGGING_CRLF();
+	PORTB ^= (1 << LED1_PORT);	// Flip the bit
 }
 
 void scheduler_task3(scheduler_status_p scheduler) {
-	DEBUGGING_VARU("task3, tick", (*scheduler).tick); DEBUGGING_CRLF();
-	PORTB ^= (1 << LED3_PORT);	// Flip the bit
+	// DEBUGGING_VARU("task3, tick", (*scheduler).tick); DEBUGGING_CRLF();
+	PORTB ^= (1 << LED2_PORT);	// Flip the bit
 }
 
 // ----------------------------------------------------------------------------
 
 int main(void) {
-
-	// ---- Initialization ----
-	DEBUGGING_INIT();
-	DEBUGGING_CRLF(); DEBUGGING_STRINGLN("HELLO[SCHEDULER]");	// Not really needed
-
+	// ---- Init ----
+	// DEBUGGING_INIT();
+	// DEBUGGING_CRLF(); DEBUGGING_STRINGLN("HELLO[SCHEDULER]");	// Not really needed
 	scheduler_init(scheduler_func0);
 	scheduler_usertask(scheduler_task1, 1);
 	scheduler_usertask(scheduler_task2, 2);
 	scheduler_usertask(scheduler_task3, 3);
 	scheduler_reinit(SCHEDULER_TCCR0B_1024, SCHEDULER_OCR0A_MAX);
 	
-	// Setup LED, DDRB - Data Direction Register, Port B
+	// Setup LEDs, DDRB - Data Direction Register, Port B
+	DDRB |= (1 << LED0_PORT); // Set port as LED output
 	DDRB |= (1 << LED1_PORT); // Set port as LED output
 	DDRB |= (1 << LED2_PORT); // Set port as LED output
-	DDRB |= (1 << LED3_PORT); // Set port as LED output
-	// DDRB |= (1 << LED4_PORT); // 	// NOTE: Used by the OWOWOD library
-	DDRB |= (1 << LED5_PORT); // Set port as LED output
+	// DDRB |= (1 << LED3_PORT); // 	// NOTE: Used by the OWOWOD library
+	DDRB |= (1 << LED4_PORT); // Set port as LED output
 
 	// Start the scheduler
 	scheduler_start();
 
 	// ---- Main Loop ----
-	for (;;) {
-		/* NOTING TO DO HERE */
-	}
+	for (;;) { /* The infinite main loop (NOTING TO DO) */ }
 
-	return 0;
+	return 0; // Return the mandatory for the "main" function int value - "0" for success.
 }
 
 // ============================================================================
